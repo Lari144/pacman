b9076899488b083a547efbd225766d6116bb65ef
Lari144 <55692422+Lari144@users.noreply.github.com>
2023-03-21 19:06:56 +0100

    modified code for the database

--- app.py
+++ app.py
@@ -6,14 +6,44 @@ import base64
 
 app = Flask(__name__)
 
-def create_table():
+def create_tables():
     conn = sqlite3.connect("qr_codes.db")
     cursor = conn.cursor()
     cursor.execute("""
+    CREATE TABLE IF NOT EXISTS customers (
+        customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        First_Name TEXT NOT NULL,
+        Last_Name TEXT NOT NULL,
+        Address TEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        customer_id INTEGER,
+        qr_id INTEGER,
+        order_date DATE NOT NULL,
+        description TEXT NOT NULL,
+        FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+    )
+    """)
+    
+    cursor.execute("""
     CREATE TABLE IF NOT EXISTS qr_codes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        data TEXT NOT NULL,
-        qr_code TEXT NOT NULL
+        qr_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        product_id INTEGER,
+        description TEXT NOT NULL,
+        qr_code TEXT NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES products (product_id)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS locations (
+        location_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        product_id INTEGER,
+        description TEXT NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES products (product_id)
     )
     """)
     conn.commit()
@@ -22,9 +52,35 @@ def create_table():
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        data = request.form["data"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        address = request.form["address"]
+        order_date = request.form["order_date"]
+        product_description = request.form["product_description"]
+        qr_description = request.form["qr_description"]
+        location_description = request.form["location_description"]
+
+        conn = sqlite3.connect("qr_codes.db")
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO customers (First_Name, Last_Name, Address) VALUES (?, ?, ?)",
+                       (first_name, last_name, address))
+        customer_id = cursor.lastrowid
+
+        cursor.execute("INSERT INTO products (customer_id, order_date, description) VALUES (?, ?, ?)",
+                            (customer_id, order_date, product_description))
+        product_id = cursor.lastrowid
+
+        cursor.execute("INSERT INTO qr_codes (product_id, description, qr_code) VALUES (?, ?, ?)",
+                    (product_id, qr_description, ''))
+        
+        cursor.execute("INSERT INTO locations (product_id, description) VALUES (?, ?)",
+                       (product_id, location_description))
+
+        qr_id = cursor.lastrowid
+
         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-        qr.add_data(data)
+        qr.add_data(qr_description)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
 
@@ -32,9 +88,8 @@ def index():
         img.save(buf, format="PNG")
         qr_code_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
 
-        conn = sqlite3.connect("qr_codes.db")
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO qr_codes (data, qr_code) VALUES (?, ?)", (data, qr_code_base64))
+        cursor.execute("UPDATE qr_codes SET qr_code = ? WHERE qr_id = ?", (qr_code_base64, qr_id))
+
         conn.commit()
         conn.close()
 
@@ -43,5 +98,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    create_table()
+    create_tables()
     app.run(debug=True)
