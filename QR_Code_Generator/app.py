2848571f43e3b7ddeb146a7648f69cb2f55019d1
postl <postl@LAPTOP-2A544SQ1>
2023-03-23 13:11:07 +0100

    app

--- QR_Code_Generator/app.py
+++ QR_Code_Generator/app.py
@@ -54,7 +54,9 @@ def create_tables():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # Check, if Formular has been sent
     if request.method == "POST":
+        # Call Values of Formular
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         address = request.form["address"]
@@ -63,30 +65,37 @@ def index():
         qr_description = request.form["qr_description"]
         location_description = request.form["location_description"]
 
+        # Connection to Database
         conn = sqlite3.connect("qr_codes.db")
         cursor = conn.cursor()
 
+        # Insert the inserted Data for Customer in Table customers
         cursor.execute("INSERT INTO customers (First_Name, Last_Name, Address) VALUES (?, ?, ?)", #insert Data in customers
                        (first_name, last_name, address))
         customer_id = cursor.lastrowid
 
+        # Insert the inserted Data for Product in Table products
         cursor.execute("INSERT INTO products (customer_id, order_date, description) VALUES (?, ?, ?)", #insert Data in products
                             (customer_id, order_date, product_description))
         product_id = cursor.lastrowid
 
+        # Insert the inserted Data for QR-Code in Table qr_codes
         cursor.execute("INSERT INTO qr_codes (product_id, description, qr_code) VALUES (?, ?, ?)", #insert Data in qr_codes
                     (product_id, qr_description, ''))
 
+        # Insert the inserted Data for Location in Table locations
         cursor.execute("INSERT INTO locations (product_id, description) VALUES (?, ?)", #insert Data in locations
                        (product_id, location_description))
 
-        qr_id = cursor.lastrowid #get qr_id
+        qr_id = cursor.lastrowid # call qr_id
 
+        # Create QR-Code with QRCode-Library
         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
         qr.add_data(qr_description)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
 
+        # Save images of QR-Code
         buf = io.BytesIO()
         img.save(buf, format="PNG")
         qr_code_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
